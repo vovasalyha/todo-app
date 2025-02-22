f7f172bdd115e5416f4e500d9e7f372f143a0aaf
volodymyr <salyha355@gmail.com>
2025-02-22 18:44:48 +0200

    Migrate UI to Material 3

--- src/main/kotlin/TodoApp.kt
+++ src/main/kotlin/TodoApp.kt
@@ -1,7 +1,7 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Visibility
